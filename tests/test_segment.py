36af6b5ea2dcbd8a0a9a0d72c53b909cd1efb5d9
KireStudios <erik.ventura.gili@gmail.com>
2025-04-10 21:09:35 +0200

    Release v1

---
+++ tests/test_segment.py
@@ -0,0 +1,9 @@
+from segment import *
+from node import *
+n1 = Node ('aaa', 0, 0) 
+n2 = Node ('bbb', 3, 4)
+n3 = Node ('ccc', -3, -4)
+s1 = Segment ('ab',n1,n2)
+s2 = Segment ('bc',n2,n3)
+print(s1.__dict__)
+print(s2.__dict__)
